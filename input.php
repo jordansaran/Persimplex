e560d640e98e33e29ca517033ac174243ddc62df
Jordan Ferreira Saran <jfsaran@gmail.com>
2017-06-06 16:46:49 -0300

    View Problema.
    Signed-off-by: Jordan Ferreira Saran <jfsaran@gmail.com>

--- input.php
+++ input.php
@@ -1,35 +1,39 @@
-<?php   require_once 'funcao.php';
-    $decisoes = 2;
-    $restricoes = 4;
+<?php
+    require_once 'funcao.php';
+
+    $decisoes = $_POST['decisoes'];
+    $restricoes = $_POST['restricoes'];
 ?>
 
 <?php include ('header.php'); ?>
 <main>
-    <div id="page2" class="container">
-        <div id="principal" class="container z-depth-5">
-            <div class="row col s12">
-                <div class="input-field col s12">
-                    <select name="funcao" id="funcao">
-                        <option value="1" selected>Maximizar</option>
-                    </select>
-                    <label for="funcao">Qual o objetivo da função ?</label>
+    <form action="problem.php" method="post">
+        <div id="page2" class="container">
+            <div id="principal" class="container z-depth-5">
+                <div class="row col s12">
+                    <div class="input-field col s12">
+                        <select name="funcao" id="funcao">
+                            <option value="1" selected>Maximizar</option>
+                        </select>
+                        <label for="funcao">Qual o objetivo da função ?</label>
+                    </div>
                 </div>
-            </div>
-            <div class="row col s12">
-                <label class="col s1">Função:</label>
-                <?php getFuncao($decisoes); ?>
-            </div>
-            <div class="row col s12">
-                <label cclass="col s1" >Restrições:</label>
-                <?php getRestricao($decisoes, $restricoes); ?>
-            </div>
-            <div class="row col s12">
-                <button id="btnSolucionar" class="btn waves-effect waves-light col s4 blue right" type="button" name="solucionar">Solucionar problema
-                    <i class="material-icons right">send</i>
-                </button>
-                <a id="btnVoltar" class="btn waves-effect waves-light col s4 red left" href="index.php" name="voltar">Voltar</a>
+                <div class="row col s12">
+                    <label class="col s1">Função:</label>
+                    <?php getFuncao($decisoes); ?>
+                </div>
+                <div class="row col s12">
+                    <label cclass="col s1" >Restrições:</label>
+                    <?php getRestricao($decisoes, $restricoes); ?>
+                </div>
+                <div class="row col s12">
+                    <button id="btnSolucionar" class="btn waves-effect waves-light col s4 blue right" type="submit" name="action">Solucionar problema
+                        <i class="material-icons right">send</i>
+                    </button>
+                    <a id="btnVoltar" class="btn waves-effect waves-light col s4 red left" href="index.php" name="voltar">Voltar</a>
+                </div>hero
             </div>
         </div>
-    </div>
+    </form>
 </main>
 <?php include ('footer.php'); ?>
---
+++ problem.php
@@ -0,0 +1,5 @@
+<?php include 'header.php'; ?>
+<main>
+    <h1 class="center"><i class="large material-icons">warning</i> Em construção.</h1>
+</main>
+<?php include 'footer.php'; ?>
